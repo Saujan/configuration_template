1b77cb3243de5d266e8d76a56328b85a0b3f9db6
Saujan Maka <saujan@whitehatengineering.com>
2021-01-16 10:02:56 +0545

    integrate home page

---
+++ app/controllers/office_controller.rb
@@ -0,0 +1,4 @@
+class OfficeController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'office/home'
+  get '/', to: 'office#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
