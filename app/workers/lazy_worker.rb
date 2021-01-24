fa98ded0bacfb3c7e70e6878cafb802ef142e6ef
Saujan Maka <saujan@whitehatengineering.com>
2021-01-24 14:48:25 +0545

    created some workers

---
+++ app/workers/lazy_worker.rb
@@ -0,0 +1,20 @@
+class LazyWorker
+    include Sidekiq::Worker
+    sidekiq_options retry: 1
+  
+    def perform(lazy_type)
+        case lazy_type
+        when 'hard'
+            while true do
+                sleep 3
+                puts 'stuck in loop'
+            end
+        when 'medium'
+            sleep 5
+            puts 'that was quite bit of work'
+        else
+            sleep 3
+            puts 'that was not a lot of work'
+        end
+    end
+end
--- app/workers/prime_mine_worker.rb
+++ app/workers/prime_mine_worker.rb
@@ -1,8 +1,10 @@
 class PrimeMineWorker
     include Sidekiq::Worker
+    sidekiq_options retry: 0
   
     def perform(prime_position)
       # do something
+      raise 'error calculation prime value'
       puts prime_position
     end
-  end
+end
