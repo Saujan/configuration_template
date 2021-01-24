63dc322781b472676f632d9cadbeebdc38a94c2d
Saujan Maka <saujan@whitehatengineering.com>
2021-01-24 21:03:56 +0545

    set metadata in honeybadger context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action do
+        Honeybadger.context({
+          user_id: 1,
+          user_email: 'saujan@gmail.com'
+        })
+    end
 end
